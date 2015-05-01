7d715aee4790c6fe1336094c37484f9d12433965
Debasish Ghosh <dghosh@acm.org>
2015-05-01 20:40:22 +0530

    start code for ch6

---
+++ src/main/scala/frdomain/ch6/domain/package.scala
@@ -0,0 +1,11 @@
+package frdomain.ch6
+package domain
+
+import scala.concurrent.Future
+import scalaz._
+import Scalaz._
+
+package object service {
+  // type Valid[A] = NonEmptyList[String] \/ A
+  type Valid[A] = EitherT[Future, NonEmptyList[String], A]
+}
